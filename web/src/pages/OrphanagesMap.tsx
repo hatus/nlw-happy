3b8267dcf5cd07d559f73237ddd23721c310f206
Hatus Níwman <hatusn@gmail.com>
2020-10-16 16:29:40 -0300

    criação da pagina map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map } from 'react-leaflet';
+
+import '../styles/pages/orphanages-map.css';
+import mapMarkerImg from '../images/map-marker.svg';
 
 function OrphanagesMap() {
-  return <h1>Hello world</h1>;
+  return (
+    <div id='page-map'>
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt='Happy' />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+        <footer>
+          <strong>Teresina</strong>
+          <span>Piauí</span>
+        </footer>
+      </aside>
+      <div></div>
+      <Link to='' className='create-orphanage'>
+        <FiPlus size={32} color='#fff' />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
